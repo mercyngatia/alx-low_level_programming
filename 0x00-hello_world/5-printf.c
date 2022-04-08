ab0093bf0190bc9a5bb053f7c1cc69c59b0b595f
mercyngatia <nyawirahngatia@gmail.com>
2022-04-08 04:58:45 -0700

    Wrote a C program that prints (With proper.....)

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - main entry
  *
- * Retturn: 
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
