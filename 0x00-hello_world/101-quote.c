8585f94afc2c4c8a6e876567e24e6efa42e1a3d6
mercyngatia <nyawirahngatia@gmail.com>
2022-04-08 05:42:40 -0700

    Wrote a C program that prints and that piece of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - main entry
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
