f337f5b38ef081e511661a854a3476689410d6c0
mercyngatia <nyawirahngatia@gmail.com>
2022-04-10 12:29:38 -0700

    Program printing the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main - main entry
+ * Desc: Get random number and check its last digit, compare with 5
+ * Return: 0
+ */
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return(0);
+}
