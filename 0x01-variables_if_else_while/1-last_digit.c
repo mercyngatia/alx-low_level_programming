93cbd1e5ca907c657c02537a6fdb99a9f6db9be2
mercyngatia <nyawirahngatia@gmail.com>
2022-04-10 11:50:57 -0700

    Program getting a random number and check its last digit, compare it with 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,16 +10,18 @@ int main(void)
 
 {
 int n;
+int lastn;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last = n % 10;
+lastn = n % 10;
 
-if (last > 5)
-printf("Last digit of %1 is %i and is greater than 5\n", n, last);
-else if (last == 0)
-printf("Last digit of %i is %i and is 0\n", n, last);
-else if(last < 6)
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+if (lastn > 5)
+printf("Last digit of %1 is %i and is greater than 5\n", n, lastn);
+else if (lastn == 0)
+printf("Last digit of %i is %i and is 0\n", n, lastn);
+else if(lastn < 6)
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastn);
 
 return (0);
 }
