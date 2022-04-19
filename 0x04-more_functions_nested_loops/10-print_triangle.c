325422beafd136455df7049e3403e43800dc4a69
mercyngatia <nyawirahngatia@gmail.com>
2022-04-19 10:32:23 -0700

    Wrote a function that prints a triangle, then new line

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangles
+ * @size: prints size
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+
+int i;
+int z;
+int d;
+int p;
+
+if (size > 0)
+{
+d = size - 1;
+for (i = 0; i < size; i++)
+{
+for (z = d; z > 0; z--)
+{
+_putchar (' ');
+}
+
+for (p = 0; p <= i; p++)
+{
+_putchar (35);
+}
+
+d--;
+
+_putchar ('\n');
+
+}
+
+}
+
+else
+{
+_putchar ('\n');
+}
+}
+
