a8eb7c556036a928de2766ef1d0489a99d857fc0
mercyngatia <nyawirahngatia@gmail.com>
2022-04-19 09:39:01 -0700

    Wrote a function that prints a square, then new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_square - check for a digit, prints a square
+ * @size: print size
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+
+int y;
+int z;
+
+if (size > 0)
+{
+for (y = 0; y < size; i++)
+{
+
+for (z = 0; z < size; z++)
+{
+_putchar (35);
+}
+_putchar ('\n');
+
+}
+
+}
+
+else
+{
+_putchar ('\n');
+}
+
+}
