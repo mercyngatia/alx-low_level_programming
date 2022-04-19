c7949c23ab25651313af7ccc84fa771bb27f3b8d
mercyngatia <nyawirahngatia@gmail.com>
2022-04-19 07:35:45 -0700

    Wrote a unction that checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,10 @@
 
 /**
  * _isupper - checks i the value is uppercase
- * #c: is the int that will use or the argument of the function
+ * @c: is the int that will use or the argument of the function
  * Return: 1 if uppercase, 0 if not
  */
+
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'z')
